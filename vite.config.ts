b548b5c6e3f91c78dbc3b93426297ead62c9dc0f
Farhan Mohd. Fokrul Alam <metalhead901@live.com>
2024-03-24 16:21:09 +0600

    added bundle analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,21 @@
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react(),
+        visualizer({
+            template: 'treemap',
+            open: true,
+            gzipSize: true,
+            brotliSize: true,
+            filename: 'bundle/index.html',
+            title: 'Bundle analyzer'
+        }),
+    ],
     resolve: {
         alias: {
             '@': path.resolve(__dirname, './src'),
